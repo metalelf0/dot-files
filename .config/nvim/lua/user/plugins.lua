29635a2e2ba9b12f4fae7ccd100d629869273490
Andrea Schiavini <metalelf0@gmail.com>
2022-04-29 08:59:43 +0200

    Update nvim config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -71,7 +71,7 @@ return packer.startup(function(use)
   use "lukas-reineke/indent-blankline.nvim"
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
-  use "max397574/which-key.nvim"
+  use "folke/which-key.nvim"
   -- use "stevearc/aerial.nvim"
   -- use { "~/sources/aerial.nvim" }
   -- use "simrat39/symbols-outline.nvim"
