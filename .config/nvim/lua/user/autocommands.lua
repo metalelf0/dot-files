b53976358c171070b1afd9e3dbafc9609d560acb
Andrea Schiavini <metalelf0@gmail.com>
2022-08-28 11:29:55 +0200

    Sun Aug 28 11:29:55 CEST 2022

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -3,6 +3,10 @@ vim.cmd([[
   autocmd!
   autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+
+  function MarkdownHighlights()
+    syntax region Label start=+==+ end=+==+
+  endfunction
 ]])
 
 local general_group = vim.api.nvim_create_augroup('general', { clear = true})
@@ -17,7 +21,8 @@ vim.api.nvim_create_autocmd('FileType', { command = 'setlocal spell', group = gi
 
 local markdown_group = vim.api.nvim_create_augroup('markdown', { clear = true })
 vim.api.nvim_create_autocmd('FileType', { command = 'setlocal wrap', group = markdown_group, pattern = 'markdown' })
-vim.api.nvim_create_autocmd('FileType', { command = 'setlocal spell', group = markdown_group, pattern = 'markdown' })
+vim.api.nvim_create_autocmd('FileType', { command = 'setlocal nospell', group = markdown_group, pattern = 'markdown' })
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, { command = 'call MarkdownHighlights()', group = markdown_group, pattern = "*.md" })
 
 local auto_resize_group = vim.api.nvim_create_augroup('auto_resize', { clear = true })
 vim.api.nvim_create_autocmd('VimResized', { command = 'tabdo wincmd = ', group = auto_resize_group })
