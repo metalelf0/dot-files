d884a4c4de5f7655862aada0102470afff174426
Andrea Schiavini <metalelf0@gmail.com>
2022-12-28 14:03:51 +0100

    feat: add matchup, refactor leap, neo-tree

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -107,6 +107,13 @@ return {
 		},
 	},
 	{
+		"andymass/vim-matchup",
+		event = "BufReadPost",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+		end,
+	},
+	{
 		"lukas-reineke/headlines.nvim",
 		ft = { "norg", "org", "markdown" },
 		config = function()
---
+++ .config/nvim/lua/user/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	cmd = "Neotree",
+	config = function()
+		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+		require("neo-tree").setup({
+			filesystem = {
+				follow_current_file = true,
+				hijack_netrw_behavior = "open_current",
+			},
+		})
+	end,
+}
