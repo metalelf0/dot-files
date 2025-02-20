3ad26c1fd8e94ae73c995b0f500b4a963ebf245b
metalelf0 <metalelf0@gmail.com>
2025-02-20 12:45:58 +0100

    Thu Feb 20 12:45:58 CET 2025

--- .config/nvim/lua/user/fun.lua
+++ .config/nvim/lua/user/fun.lua
@@ -3,31 +3,4 @@ local core = require("lib/core")
 
 M.set_keymaps = function() end
 
-M.update_tmux_theme = function()
-	local colorscheme = vim.api.nvim_exec("colorscheme", true)
-	local config_path = vim.fn.expand("~/.config/tmux/tmux.conf")
-
-	local lines = {}
-	for line in io.lines(config_path) do
-		if line:find(".palette.tmux") then
-			filepath = core.get_homedir() .. "/.config/tmux/themes/" .. colorscheme .. ".palette.tmux"
-			if vim.fn.filereadable(vim.fn.expand(filepath)) == 1 then
-				line = "source-file " .. filepath
-				vim.notify("Tmux palette is now " .. filepath)
-			else
-				vim.notify(
-					"No palette file found for colorscheme " .. colorscheme .. ", would expect one at " .. filepath
-				)
-			end
-		end
-		table.insert(lines, line)
-	end
-
-	local file = io.open(config_path, "w")
-	for _, line in ipairs(lines) do
-		file:write(line .. "\n")
-	end
-	file:close()
-end
-
 return M
