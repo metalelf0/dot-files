df16047fb901e05c17e73b143c14393c219c8e63
metalelf0 <metalelf0@gmail.com>
2026-02-28 14:24:06 +0100

    fix: remove more unneeded plugins

--- .config/nvim/lua/user/plugins/lush.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"rktjmp/lush.nvim",
-	event = "VeryLazy",
-	-- dependencies = {
-	-- 	"rktjmp/shipwright.nvim",
-	-- },
-	-- if you wish to use your own colorscheme:
-	-- { dir = '/absolute/path/to/colorscheme', lazy = true },
-}
